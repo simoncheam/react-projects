6925aaafcedd4e076e4a39531ce9e65d92cdad80
Simon Cheam <simoncheam@Simons-MacBook-Pro.local>
2022-04-19 20:02:45 -0400

    reviews project setup complete and working

--- 03-reviews/setup/src/App.js
+++ 03-reviews/setup/src/App.js
@@ -1,7 +1,20 @@
 import React from 'react';
 import Review from './Review';
+import { FaGithubSquare } from 'react-icons/fa';
 function App() {
-  return <h2>reviews project setup</h2>;
+  return (
+    <main>
+      <div className="container">
+        <div className="title">
+          <h2>Our Reviews</h2>
+          <div className='underline'></div>
+
+        </div>
+        <Review/>
+      </div>
+      {/* <FaGithubSquare className='icon'/> */}
+    </main>
+  );
 }
 
 export default App;
