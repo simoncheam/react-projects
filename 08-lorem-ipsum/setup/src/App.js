0cd5ead5a732f08f43ffdc4f8cfb16bf77fcc3e8
Simon Cheam <simoncheam@Simons-MacBook-Pro.local>
2022-04-21 11:26:52 -0400

    lorem ipsum complete - ideal for displaying selected amount of data

--- 08-lorem-ipsum/setup/src/App.js
+++ 08-lorem-ipsum/setup/src/App.js
@@ -1,11 +1,51 @@
 import React, { useState } from 'react';
 import data from './data';
 function App() {
-  
-  
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let amount = parseInt(count);
+
+    if (count <= 0) {
+      //alert('Please enter a number greater than 0');
+      amount = 1;
+    }
+
+    if (count > data.length) {
+      amount = data.length;
+    }
+
+    if (amount > 0) {
+      setText(data.slice(0, amount));
+    }
+  };
+
   return (
-  <h2>lorem ipsum project setup</h2>
-    )
+    <section className="section-center">
+      <h3>tired of boring lorem ipsum?</h3>
+      <form className="lorem-form" onSubmit={handleSubmit}>
+        <label htmlFor="amount">paragraphs:</label>
+        <input
+          type="number"
+          id="amount"
+          min="1"
+          max={data.length}
+          value={count}
+          onChange={(e) => setCount(e.target.value)}
+        />
+        <button type="submit" className="btn">
+          generate
+        </button>
+      </form>
+      <article className="lorem-text">
+        {text.map((item, index) => (
+          <p key={index}>{item}</p>
+        ))}
+      </article>
+    </section>
+  );
 }
 
 export default App;
