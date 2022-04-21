c9c8b7d6f448146012580082c8f2673c20aed3e6
Simon Cheam <simoncheam@Simons-MacBook-Pro.local>
2022-04-21 10:36:49 -0400

    slider and alternative slider working - ideal for reviews/testimonial section

--- 07-slider/setup/src/Alternative.js
+++ 07-slider/setup/src/Alternative.js
@@ -6,6 +6,26 @@ function App() {
   const [people, setPeople] = useState(data);
   const [index, setIndex] = useState(0);
 
+  const nextSlide = () => {
+    setIndex((oldIndex) => {
+      let index = oldIndex + 1;
+      if (index > people.length - 1) {
+        index = 0;
+      }
+      return index;
+    });
+  };
+
+  const prevSlide = () => {
+    setIndex((oldIndex) => {
+      let index = oldIndex - 1;
+      if (index < 0) {
+        index = people.length - 1;
+      }
+      return index;
+    });
+  };
+
   useEffect(() => {
     const lastIndex = people.length - 1;
 
@@ -20,7 +40,14 @@ function App() {
 
   useEffect(() => {
     let slider = setInterval(() => {
-      setIndex(index + 1);
+      setIndex((oldIndex) => {
+        let index = oldIndex + 1;
+        if (index > people.length - 1) {
+          index = 0;
+        }
+
+        return index;
+      });
     }, 3000);
     return () => clearInterval(slider);
   }, [index]);
@@ -58,10 +85,10 @@ function App() {
             </article>
           );
         })}
-        <button className="prev" onClick={() => setIndex(index - 1)}>
+        <button className="prev" onClick={prevSlide}>
           <FiChevronLeft />
         </button>
-        <button className="next" onClick={() => setIndex(index + 1)}>
+        <button className="next" onClick={nextSlide}>
           <FiChevronRight />
         </button>
       </div>
