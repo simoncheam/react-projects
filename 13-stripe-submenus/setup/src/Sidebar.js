f008aa21d4b470fd60baa5798d371a47199a9c4b
Simon Cheam <simoncheam@Simons-MacBook-Pro.local>
2022-04-23 12:06:10 -0400

    dynamic navbar submenu display working - using global context, getBoundingClientRect, useRef, useEffect to get coordinates by location

--- 13-stripe-submenus/setup/src/Sidebar.js
+++ 13-stripe-submenus/setup/src/Sidebar.js
@@ -1,9 +1,40 @@
-import React from 'react'
-import { FaTimes } from 'react-icons/fa'
-import sublinks from './data'
+import React from 'react';
+import { FaTimes } from 'react-icons/fa';
+import sublinks from './data';
+import { useGlobalContext } from './context';
 
 const Sidebar = () => {
-  return <h2>sidebar component</h2>
-}
+  const { isSidebarOpen, closeSidebar } = useGlobalContext();
 
-export default Sidebar
+  return (
+    <aside className={`${isSidebarOpen ? 'sidebar-wrapper show' : 'sidebar-wrapper'}`}>
+      <div className="sidebar">
+        <button className="close-btn" onClick={closeSidebar}>
+          <FaTimes />
+        </button>
+        <div className='sidebar-links'>
+          {sublinks.map(({ page, links }, index) => (
+            <article key={index}>
+                <h4>{page}</h4>
+                <div className='sidebar-sublinks'>
+                  {links.map(({ label, url, icon }, index) => (
+
+                    <a key={index} href={url}>
+                      {icon}
+                      {label}
+                    </a>
+                    )
+                  )}
+                </div>
+            </article>
+          ))}
+
+        </div>
+
+      </div>
+      sidebar component
+    </aside>
+  );
+};
+
+export default Sidebar;
--- 13-stripe-submenus/setup/src/context.js
+++ 13-stripe-submenus/setup/src/context.js
@@ -1,2 +1,50 @@
-import React, { useState, useContext } from 'react'
-import sublinks from './data'
+import React, { useState, useContext } from 'react';
+import sublinks from './data';
+
+// setup global context
+
+const AppContext = React.createContext();
+
+export const AppProvider = ({ children }) => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [isSubmenuOpen, setIsSubmenuOpen] = useState(true);
+  const [location, setLocation] = useState({});
+  //submenu function
+
+  //open submenu
+
+  const openSidebar = () => {
+    setIsSidebarOpen(true);
+  };
+
+  const closeSidebar = () => {
+    setIsSidebarOpen(false);
+  };
+  const openSubmenu = (text, coordinates) => {
+    setLocation(coordinates);
+    setIsSubmenuOpen(true);
+  };
+
+  const closeSubmenu = () => {
+    setIsSubmenuOpen(false);
+  };
+
+  return (
+    <AppContext.Provider
+      value={{
+        isSidebarOpen,
+        isSubmenuOpen,
+        openSidebar,
+        closeSidebar,
+
+        openSubmenu,
+        closeSubmenu,
+        location
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+export const useGlobalContext = () => useContext(AppContext);
