8de62816ee8616ab6e3f12a26167f32d536f1cfd
Simon Cheam <simoncheam@Simons-MacBook-Pro.local>
2022-04-23 17:03:42 -0400

    refactored with toggleAmount to condense increase/decrease

--- 14-cart/setup/src/context.js
+++ 14-cart/setup/src/context.js
@@ -1,71 +1,62 @@
-import React, { useState, useContext, useReducer, useEffect } from 'react'
-import cartItems from './data'
-import reducer from './reducer'
+import React, { useState, useContext, useReducer, useEffect } from 'react';
+import cartItems from './data';
+import reducer from './reducer';
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
-const url = 'https://course-api.com/react-useReducer-cart-project'
-const AppContext = React.createContext()
+const url = 'https://course-api.com/react-useReducer-cart-project';
+const AppContext = React.createContext();
 
 const initialState = {
   loading: false,
   cart: cartItems,
   total: 0,
   amount: 0,
-}
+};
 
 const AppProvider = ({ children }) => {
-
-
   // looks for reducer function and initial state
-  const [state, dispatch] = useReducer(reducer, initialState)
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   // * remove items from cart
   const clearCart = () => {
-    dispatch({ type: 'CLEAR_CART' })
-  }
-
+    dispatch({ type: 'CLEAR_CART' });
+  };
 
   // * remove single item from cart
-const remove = (id) => {
-  dispatch({ type: 'REMOVE', payload: id })
+  const remove = (id) => {
+    dispatch({ type: 'REMOVE', payload: id });
+  };
 
-}
+  const increase = (id) => {
+    dispatch({ type: 'INCREASE', payload: id });
+  };
+  const decrease = (id) => {
+    dispatch({ type: 'DECREASE', payload: id });
+  };
+  const fetchData = async () => {
+    dispatch({ type: 'LOADING' });
+    const response = await fetch(url);
+    const data = await response.json();
+    dispatch({
+      type: 'DISPLAY_ITEMS',
+      payload: data,
+    });
+  };
 
-const increase = (id) => {
+  const toggleAmount = (id, type) => {
+    dispatch({
+      type: 'TOGGLE_AMOUNT',
+      payload: { id, type },
+    });
+  };
 
-  dispatch({ type: 'INCREASE', payload: id })
-}
-const decrease = (id) => {
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-  dispatch({ type: 'DECREASE', payload: id })
-}
-const fetchData = async () => {
-
-  dispatch({ type: 'LOADING' })
-  const response = await fetch(url)
-  const data = await response.json()
-  dispatch({
-    type: 'DISPLAY_ITEMS',
-    payload: data})
-}
-
-useEffect(() => {
-
-  fetchData()
-}, [])
-
-
-
-useEffect(() => {
-
-  dispatch({ type: 'GET_TOTALS' })
-
-
-}, [state.cart])
-
-
-
-
+  useEffect(() => {
+    dispatch({ type: 'GET_TOTALS' });
+  }, [state.cart]);
 
   return (
     <AppContext.Provider
@@ -74,16 +65,17 @@ useEffect(() => {
         clearCart,
         remove,
         increase,
-        decrease
+        decrease,
+        toggleAmount,
       }}
     >
       {children}
     </AppContext.Provider>
-  )
-}
+  );
+};
 // make sure use
 export const useGlobalContext = () => {
-  return useContext(AppContext)
-}
+  return useContext(AppContext);
+};
 
-export { AppContext, AppProvider }
+export { AppContext, AppProvider };
