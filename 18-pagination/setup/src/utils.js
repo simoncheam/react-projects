9d63f4c455d5853d7ec2ef3ee6dff1ef68fd107f
Simon Cheam <simoncheam@Simons-MacBook-Pro.local>
2022-04-25 19:24:55 -0400

    pagination complete

--- 18-pagination/setup/src/utils.js
+++ 18-pagination/setup/src/utils.js
@@ -1,3 +1,16 @@
-const paginate = () => {}
+const paginate = (followers) => {
+//   console.log(followers);
 
-export default paginate
+  const itemsPerPage = 10;
+  const pages = Math.ceil(followers.length / itemsPerPage); // # pages we need
+
+  // creating new array with items per page, setting up array of arrays for pagination
+  const newFollowers = Array.from({ length: pages }, (_, index) => {
+    const start = index * itemsPerPage;
+    return followers.slice(start, start + itemsPerPage);
+  });
+
+ return newFollowers;
+};
+
+export default paginate;
