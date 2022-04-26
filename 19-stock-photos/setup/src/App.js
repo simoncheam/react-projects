2bc90b0460d3917297b7a62ecc8522b1bbbf927a
Simon Cheam <simoncheam@Simons-MacBook-Pro.local>
2022-04-26 10:35:26 -0400

    bug fix + refactoring scroll functionality - all appears to be working

--- 19-stock-photos/setup/src/App.js
+++ 19-stock-photos/setup/src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { useRef } from 'react';
 import { FaSearch } from 'react-icons/fa';
 import Photo from './Photo';
 const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`;
@@ -9,95 +10,111 @@ const searchUrl = `https://api.unsplash.com/search/photos/`;
 function App() {
   const [loading, setLoading] = useState(false);
   const [photos, setPhotos] = useState([]);
-  const [page, setPage] = useState(0);
-  const [queryParam, setQueryParam] = useState('')
+  const [page, setPage] = useState(1);
+  const [queryParam, setQueryParam] = useState('');
+  const mounted = useRef(false);
+  const [newImages, setNewImages] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    setPage(1) // ! resets page value
 
+    if (!queryParam) return;
 
+    if (page === 1) {
+      fetchImages();
+      return;
+    }
+
+    setPage(1); // ! resets page value
   };
 
   // page change -->  refetch
   useEffect(() => {
     fetchImages();
+    //eslint-disable-next-line react-hooks/exhaustive-deps
   }, [page]);
+
+
+  // ! 2nd UE - only run after initial render
+  useEffect(() => {
+    if (!mounted.current) {
+      mounted.current = true;
+      return;
+    }
+
+    console.log('second ');
+    if(!newImages) return;
+    if(loading) return;
+
+    setPage((oldPage) => oldPage+1)
+
+
+  }, [newImages]);
+
+  // ! refactored scrolling functionality
+
+  const event = () => {
+    if (window.innerHeight + window.scrollY >= document.body.scrollHeight - 2) {
+      setNewImages(true);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', event);
+
+    return () => window.removeEventListener('scroll', event);
+  }, []);
+
+
 
   const fetchImages = async () => {
     setLoading(true);
 
     let url;
     const urlPage = `&page=${page}`; //page url
-    const urlQuery = `&query=${queryParam}`
+    const urlQuery = `&query=${queryParam}`;
 
-    if(queryParam){
-
-      url = ` ${searchUrl}${clientID}${urlPage}${urlQuery}`
-    }else{
-
+    if (queryParam) {
+      url = ` ${searchUrl}${clientID}${urlPage}${urlQuery}`;
+    } else {
       url = `${mainUrl}${clientID}${urlPage}`; // * main url
     }
-
-
 
     try {
       const response = await fetch(url);
       const data = await response.json();
 
-      console.log(data)
+      // console.log(data);
 
-      setPhotos((oldPhotos)=>{
-
-        if(queryParam && page ===1){
-          return data.results
-        }else if(queryParam){
-
-          return [...oldPhotos, ...data.results]
-
-        }else{
-
-          return [...oldPhotos, ...data]
-
+      setPhotos((oldPhotos) => {
+        if (queryParam && page === 1) {
+          return data.results;
+        } else if (queryParam) {
+          return [...oldPhotos, ...data.results];
+        } else {
+          return [...oldPhotos, ...data];
         }
       });
-
-
+      setNewImages(false);
       setLoading(false);
-
-
     } catch (error) {
+      setNewImages(false);
       setLoading(false);
       console.log(error);
     }
   };
 
-  useEffect(() => {
-    const event = window.addEventListener('scroll', () => {
-      if (!loading && window.innerHeight + window.scrollY >= document.body.scrollHeight - 2) {
-        console.log('END! - fetch new images');
-
-        setPage((oldPage) => { //? where does the old page come from?
-          return oldPage + 1;
-        });
-      }
-      // check innder hight, scroll amount
-      // console.log(`inner height ${window.innerHeight}`);
-      // console.log(`scrollY: ${window.scrollY}`);
-      // console.log(`bodyHeight ${document.body.scrollHeight}`);
-
-      // eslint-disable-next-line
-    });
-
-    return () => window.removeEventListener('scroll', event);
-  }, []);
-
   return (
     <main>
       <section className="search">
         <form className="search-form">
-          <input className="form-input" type="text" placeholder="search" value={queryParam}
-          onChange={(e)=>setQueryParam(e.target.value)} />
+          <input
+            className="form-input"
+            type="text"
+            placeholder="search"
+            value={queryParam}
+            onChange={(e) => setQueryParam(e.target.value)}
+          />
           <button className="submit-btn" type="submit" onClick={handleSubmit}>
             <FaSearch />
           </button>
@@ -116,3 +133,26 @@ function App() {
 }
 
 export default App;
+
+//removed:
+
+// useEffect(() => {
+//   const event = window.addEventListener('scroll', () => {
+//     if (!loading && window.innerHeight + window.scrollY >= document.body.scrollHeight - 2) {
+//       console.log('END! - fetch new images');
+
+//       setPage((oldPage) => { //? where does the old page come from?
+//         return oldPage + 1;
+//       });
+//     }
+//     // check innder hight, scroll amount
+//     // console.log(`inner height ${window.innerHeight}`);
+//     // console.log(`scrollY: ${window.scrollY}`);
+//     // console.log(`bodyHeight ${document.body.scrollHeight}`);
+
+//   });
+
+//   return () => window.removeEventListener('scroll', event);
+//   // eslint-disable-next-line
+
+// }, []);
